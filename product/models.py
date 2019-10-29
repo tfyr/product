952b0134a8cbd3584007b6b24633a6a9fb013161
nash <nash34@gmail.com>
2019-10-29 19:14:22 +0500

    init

---
+++ product/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+MEASURE_CHOICES = (
+    (0, u'гр.'),
+    (1, u'мл.'),
+)
+
+class Item(models.Model):
+    class Meta:
+        verbose_name = "Позиция"
+        verbose_name_plural = "Позиции"
+    name = models.CharField(verbose_name="Наименование", max_length=250)
+    weight = models.PositiveIntegerField(verbose_name="Вес", default=None, blank=True, null=True)
+    measure = models.PositiveIntegerField('Ед. измерения', default=0, blank=False, null=False, choices=MEASURE_CHOICES)
+    descr = models.CharField(verbose_name="Описание", max_length=5250)
+    sostav = models.CharField(verbose_name="Состав", max_length=5250)
+    price = models.PositiveIntegerField(verbose_name="Цена", default=None, blank=True, null=True)
+    oldprice = models.PositiveIntegerField(verbose_name="Перечеркнутая цена", default=None, blank=True, null=True)
+
+    href = models.CharField(verbose_name="код для ссылки", max_length=64, unique=True, null=False, blank=False)
+
+
+    def __str__(self):
+        return str(self.name)
+
